257ce3adbeee4049c3b1527c9ad05718c47f1bf3
Namozbey <nostonayev@gmail.com>
2022-07-10 12:24:53 +0500

    add AuthLayout

--- src/constants/basic.js
+++ src/constants/basic.js
@@ -1,2 +1,4 @@
 export const errorImageUrl =
   "https://bitsofco.de/content/images/2018/12/broken-1.png";
+export const loginImageUrl =
+  "https://eg.almaviva-visa.services/assets/images/user/signup.svg";
---
+++ src/layouts/AuthLayout/index.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/display-name */
+import "./style.scss";
+import React from "react";
+import { loginImageUrl } from "../../constants/basic";
+interface Props {
+  component: React.FC;
+}
+
+type options = {
+  [key: string]: string | number | boolean;
+};
+
+const Layout = (props: Props) => {
+  const { component: Component } = props;
+  return (
+    <div className="auth-layout">
+      <div className="left-side">
+        <img src={loginImageUrl} alt="login-image" />
+      </div>
+      <div className="right-side">
+        <Component />
+      </div>
+    </div>
+  );
+};
+
+export default function authLayout(component: React.FC, options?: options) {
+  return () => <Layout component={component} {...options} />;
+}
