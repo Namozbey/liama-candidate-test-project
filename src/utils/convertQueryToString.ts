51e77120802f5c05a69e1b3d4efa9e7ef852b9a4
Namozbey <nostonayev@gmail.com>
2022-07-11 09:21:45 +0500

    modify convertQueryToString function

--- src/utils/convertQueryToString.ts
+++ src/utils/convertQueryToString.ts
@@ -1,15 +1,11 @@
-type RestQuery = {
-  [key: string]: string | number;
-};
-
-export default function convertQueryToString(rest: RestQuery, initial = "&") {
-  const keys = Object.keys(rest);
-  console.log(keys);
+export default function convertQueryToString(params: Params) {
+  if (typeof params !== "object") return "";
+  const keys = Object.keys(params);
 
   if (!keys.length) return "";
   return keys.reduce(
     (prev, key, i) =>
-      i === 0 ? `${prev}${key}=${rest[key]}` : `${prev}&${key}=${rest[key]}`,
-    initial
+      i === 0 ? `${key}=${params[key]}` : `${prev}&${key}=${params[key]}`,
+    ""
   );
 }
