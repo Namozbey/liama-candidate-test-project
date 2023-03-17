c478a19c88a86b3750bbf0b7a082779dd6c1ad98
Namozbey <nostonayev@gmail.com>
2023-03-18 04:30:38 +0500

    add parse-query custom hook

--- src/hooks/use-parse-query.ts
+++ src/hooks/use-parse-query.ts
@@ -1,6 +1,8 @@
 import { useMemo } from "react";
 import parseQuery from "../utils/parseQuery";
+import { useLocation } from "react-router-dom";
 
-export default function useParseQuery(query: string) {
-  return useMemo(() => parseQuery(query), [query]);
+export default function useParseQuery(query?: string) {
+  const location = useLocation();
+  return useMemo(() => parseQuery(query), [query ?? location.search]);
 }
