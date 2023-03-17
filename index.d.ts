923362a853c684a8e9fcc98d60aaff9bb93c5779
Namozbey <nostonayev@gmail.com>
2023-03-18 04:33:08 +0500

    modify ts configs

--- index.d.ts
+++ index.d.ts
@@ -6,20 +6,18 @@ declare module "*.svg";
 declare module "*.webp";
 
 type Params = {
-  [key: string]: any;
+  _sort?: string;
+  _order?: "desc" | "asc";
+  _start?: number;
+  _end?: number;
+  [key: string]: number | string | boolean | undefined;
 };
 
-type Property = {
-  id: string;
-  name: string;
-  value: string;
-};
-
-interface Variation {
+interface ProductType {
   id?: number;
-  name?: string;
-  barcode?: number;
-  properties?: Property[];
-  productProperties?: Property[];
-  [key: string]: any;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  imageSrc?: string;
 }
